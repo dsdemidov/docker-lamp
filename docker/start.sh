dd3a76cffca145942d2bb7dd51acd267d50d0496
Dmitry Demidov <mail@demidov.media>
2017-02-22 09:07:58 +0300

    Added rm to docker containers

--- docker/start.sh
+++ docker/start.sh
@@ -3,14 +3,14 @@
 
 docker build -t ${PHP_APP_IMAGE_NAME} .
 
-docker run -d --name ${DB_CNT} \
+docker run -d --rm --name ${DB_CNT} \
 	-v ${PWD}/docker-db-entrypoint:/docker-entrypoint-initdb.d \
 	-v ${PWD}/mysql:/var/lib/mysql \
 	-v ${PWD}/config/mysql:/etc/mysql/conf.d \
 	-e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
 	-e MYSQL_DATABASE=${MYSQL_DATABASE} \
 	mysql:latest
-docker run -d -p ${EXPOSED_WEB_PORT}:80 \
+docker run -d --rm -p ${EXPOSED_WEB_PORT}:80 \
 	--name ${PHP_CNT} \
 	--link ${DB_CNT}:${DB_CNT} \
 	-v ${WWW_DIR}:/var/www/html \
--- docker/stop.sh
+++ docker/stop.sh
@@ -2,5 +2,3 @@
 . $(dirname "$0")/../config/parameters.sh
 docker stop ${PHP_CNT}
 docker stop ${DB_CNT}
-docker rm ${PHP_CNT}
-docker rm ${DB_CNT}
