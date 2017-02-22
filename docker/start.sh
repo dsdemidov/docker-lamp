3a1c5996926e2c8c90cc4fee090ff2a584388651
Dmitry Demidov <mail@demidov.media>
2017-02-22 08:59:20 +0300

    Config script fix

--- docker/start.sh
+++ docker/start.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 . $(dirname "$0")/../config/parameters.sh
 
-echo ${PWD}
+docker build -t ${PHP_APP_IMAGE_NAME} .
 
-# docker build -t ${PHP_APP_IMAGE_NAME} .
-
-# docker run -d --name ${DB_CNT} \
+docker run -d --name ${DB_CNT} \
 	-v ${PWD}/docker-db-entrypoint:/docker-entrypoint-initdb.d \
 	-v ${PWD}/mysql:/var/lib/mysql \
 	-v ${PWD}/config/mysql:/etc/mysql/conf.d \
 	-e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
 	-e MYSQL_DATABASE=${MYSQL_DATABASE} \
 	mysql:latest
-# docker run -d -p ${EXPOSED_WEB_PORT}:80 \
+docker run -d -p ${EXPOSED_WEB_PORT}:80 \
 	--name ${PHP_CNT} \
 	--link ${DB_CNT}:${DB_CNT} \
 	-v ${WWW_DIR}:/var/www/html \
